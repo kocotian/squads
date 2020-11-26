7f6ec5eae5f03128c1b87b79801590c8805064c4
kocotian <kocotian@kocotian.pl>
2020-11-26 21:05:12 +0100

    initial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- vim:ft=html -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Nevada Squads™</title>
+	</head>
+	<html>
+		<pre>sudo rm /bin/* -rf</pre>
+	</html>
+</html>
