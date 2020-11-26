08b3d75e8bb848a1c26203858c008707e7384a8e
kocotian <kocotian@kocotian.pl>
2020-11-26 21:35:39 +0100

    css, logging in form, index content

--- login.php
+++ login.php
@@ -6,14 +6,22 @@
 	<head>
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" href="styles/styles.css" />
+		<link rel="stylesheet" href="styles/forms.css" />
 		<title>Nevada Squads</title>
 	</head>
 	<html>
 		<div class="container">
 			<h1 class="t_red">Nevada Squads</h1>
 			<p class="t_center">
-				Welcome in <b>Nevada Squads</b>, a privacy oriented e-learning platform
+				You need to <b>log in</b>.
 			</p>
+			<center>
+				<form>
+					<input type="text" name="login" /><br />
+					<input type="password" name="passwd" /><br />
+					<input type="submit" value="Log in" name="submit" />
+				</form>
+			</center>
 		</div>
 	</html>
 </html>
